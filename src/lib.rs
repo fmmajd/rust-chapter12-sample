95a2cd6607eab2cb3347ad3ec9119f1a466d2d18
fatemeh <fmmajd452@gmail.com>
2019-12-14 14:21:15 +0330

    replaced loops with iterators

--- src/lib.rs
+++ src/lib.rs
@@ -25,14 +25,23 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new (args: &[String]) -> Result<Config, &str>
+    pub fn new (mut args: std::env::Args) -> Result<Config, &'static str>
     {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("RUST_SAMPLE_CASE_INSENSITIVE").is_err();
 
         Ok(Config {query,filename, case_sensitive})
@@ -41,15 +50,9 @@ impl Config {
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>
 {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>
